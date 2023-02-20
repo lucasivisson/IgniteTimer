599d53724a17febaa85f336be5e5cff010902f0a
Lucas Ivisson <lucasivisson1@gmail.com>
2023-02-20 18:42:41 -0300

    🐛 fix a bug on responsivity

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  height: auto;
   margin: 5rem auto;
   padding: 2.5rem;
   background: green;
@@ -12,4 +12,8 @@ export const LayoutContainer = styled.div`
 
   display: flex;
   flex-direction: column;
+
+  @media (min-height: 700px) {
+    height: calc(100vh - 10rem);
+  }
 `

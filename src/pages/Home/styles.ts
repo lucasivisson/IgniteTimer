d7999b1ee02164ecfcebd7fb43f30a606f9ca479
Lucas Ivisson <lucasivisson1@gmail.com>
2023-02-21 11:42:01 -0300

    📱 finishing responsitivity of the timer

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,6 +6,7 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 2rem 0 0 0;
 
   form {
     display: flex;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -23,4 +23,16 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 400;
     font-size: 1rem;
   }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  @media (max-width: 425px) {
+    html {
+      font-size: 75%;
+    }
+  }
 `
